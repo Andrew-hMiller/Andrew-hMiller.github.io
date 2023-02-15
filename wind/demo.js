978bf9aec50a8cbb6d7f7861c57625182616d621
Andrew-hMiller <122642072+Andrew-hMiller@users.noreply.github.com>
2023-02-15 12:20:34 -0700

    Add files via upload

---
+++ wind/demo.js
@@ -0,0 +1,60 @@
+
+function initDemoMap(){
+
+    var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, ' +
+        'AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+
+    var Esri_DarkGreyCanvas = L.tileLayer(
+        "http://{s}.sm.mapstack.stamen.com/" +
+        "(toner-lite,$fff[difference],$fff[@23],$fff[hsl-saturation@20])/" +
+        "{z}/{x}/{y}.png",
+        {
+            attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, ' +
+            'NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+        }
+    );
+
+    var baseLayers = {
+        "Satellite": Esri_WorldImagery,
+        "Grey Canvas": Esri_DarkGreyCanvas
+    };
+
+    var map = L.map('map', {
+        layers: [ Esri_WorldImagery ]
+    });
+
+    var layerControl = L.control.layers(baseLayers);
+    layerControl.addTo(map);
+    map.setView([-22, 150], 5);
+
+    return {
+        map: map,
+        layerControl: layerControl
+    };
+}
+
+// demo map
+var mapStuff = initDemoMap();
+var map = mapStuff.map;
+var layerControl = mapStuff.layerControl;
+
+// load data (u, v grids) from somewhere
+$.getJSON('velocity.json', function (data) {
+
+	var velocityLayer = L.velocityLayer({
+		displayValues: true,
+		displayOptions: {
+			displayPosition: 'bottomleft',
+			displayEmptyString: 'No velocity data'
+		},
+		data: data, // see demo/velocity.json,
+		maxVelocity: 10 // used to align color scale
+	});
+
+	layerControl.addOverlay(velocityLayer, 'my velocity layer');
+	velocityLayer.addTo(map);
+});
+
+
